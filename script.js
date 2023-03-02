95c9ce8474d25bf79bb2580b8d758b3a300603d9
yong1le <ygle.he@gmail.com>
2023-03-02 00:36:15 -0500

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+const project = document.querySelector(".projects")
+const about = document.querySelector(".about")
+const bio = document.querySelector(".bio")
+const themeChanger = document.querySelector("#change-theme")
+
+const showProjects = () => {
+  alert("Show me projects")
+}
+
+// observer = new MutationObserver((mutationsList, observer) => {
+//   console.log(mutationsList);
+// })
+// observer.observe(bio, {characterData: false, childList: true, attributes: false})
+
+
+const bios = ["University of Toronto", "Albert Campbell"];
+const changeBio = () => {
+  bio.classList.add('pre-animation');
+  setTimeout(() => {
+    const current = bio.innerText;
+    if (bios.indexOf(current) == bios.length - 1) {
+      bio.innerHTML = bios[0];
+    }
+    else {
+      bio.innerHTML = bios[bios.indexOf(current) + 1]
+    }
+    bio.classList.remove('pre-animation');
+  }, 1000);
+}
+
+setInterval(changeBio, 5000);
+project.addEventListener('click', showProjects);
+about.addEventListener('click', showProjects);
