7bd1253445051b4c705f670c6c23d7bd5457fbe9
yong1le <ygle.he@gmail.com>
2023-03-06 12:13:00 -0500

    add linking

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-const project = document.querySelector(".projects")
-const about = document.querySelector(".about")
-const bio = document.querySelector(".bio")
-const themeChanger = document.querySelector("#change-theme")
+const project = document.querySelector(".projects");
+const about = document.querySelector(".about");
+const bio = document.querySelector(".bio");
 
 const showProjects = () => {
-  alert("Show me projects")
-}
+  window.open('./projects.html', '_self')
+};
 
 // observer = new MutationObserver((mutationsList, observer) => {
 //   console.log(mutationsList);
 // })
 // observer.observe(bio, {characterData: false, childList: true, attributes: false})
 
-
 const bios = ["University of Toronto", "Albert Campbell"];
 const changeBio = () => {
-  bio.classList.add('pre-animation');
+  bio.classList.add("pre-animation");
   setTimeout(() => {
     const current = bio.innerText;
     if (bios.indexOf(current) == bios.length - 1) {
       bio.innerHTML = bios[0];
+    } else {
+      bio.innerHTML = bios[bios.indexOf(current) + 1];
     }
-    else {
-      bio.innerHTML = bios[bios.indexOf(current) + 1]
-    }
-    bio.classList.remove('pre-animation');
+    bio.classList.remove("pre-animation");
   }, 1000);
-}
+};
 
 setInterval(changeBio, 5000);
-project.addEventListener('click', showProjects);
-about.addEventListener('click', showProjects);
+project.addEventListener("click", showProjects);
+about.addEventListener("click", showProjects);
+
