0ebb44d28e379f9827ab58e2be36a3995b6f60aa
yong1le <ygle.he@gmail.com>
2023-10-13 12:42:28 -0400

    nicer look

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,38 +2,48 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 
-	let linksRevealed = true;
-	onMount(() => {
-		window.addEventListener('resize', () => {
-			// for cross browser compatibility
-			let w =
-				window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-			// tailwind `md` breakpoint
-			if (w >= 768) {
-				linksRevealed = true;
-			} else {
-				linksRevealed = false;
-			}
-		});
-	});
+	// Auto opening/close links in mobile view
+	let navOpened = false;
+
+	// Highlighting current page in nav bar
 </script>
 
-<header class="px-10 sm:px-16 md:px-24 lg:px-40 py-16">
-	<nav class="flex flex-row justify-between relative">
-		<h1 class="text-3xl font-bold">
+<header class="px-10 sm:px-16 md:px-24 lg:px-40 pt-12 transition-all">
+	<nav class="flex flex-row justify-between items-center relative">
+		<h1 class="text-4xl text-red font-bold">
 			<a href="/">Yong Le He</a>
 		</h1>
-		<details open={linksRevealed}>
-			<summary class="inline md:hidden cursor-pointer"> Nav </summary>
-			<ul class="absolute right-0 bottom-[-60px] flex flex-col gap-4 md:static md:flex-row">
-				<li>
-					<a class="nav-link" href="/projects">Projects</a>
+		<button
+			on:click={() => {
+				navOpened = !navOpened;
+			}}
+			class="text-red md:hidden"
+		>
+			Nav
+		</button>
+		<div
+			class={`
+      ${navOpened ? '' : 'invisible opacity-0 md:visible md:opacity-100'}
+      absolute right-0 top-[50px] bg-beige py-3 px-2 rounded-lg 
+      md:static md:py-0 md:px-0 md:bg-transparent 
+      transition-all duration-200 ease-in
+      `}
+		>
+			<ul id="nav-links" class=" flex flex-col gap-4 md:flex-row">
+				<li class="hover:scale-105 transition-all">
+					<a
+						class="text-xl px-3 py-2 rounded-lg hover:bg-beige hover:text-red transition-all"
+						href="/projects">Projects</a
+					>
 				</li>
-				<li>
-					<a class="nav-link" href="/skills">Skills</a>
+				<li class="hover:scale-105 transition-all">
+					<a
+						class="text-xl px-3 py-2 rounded-lg hover:bg-beige hover:text-red transition-all"
+						href="/skills">Skills</a
+					>
 				</li>
 			</ul>
-		</details>
+		</div>
 	</nav>
 </header>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Poppins", 'sans-serif']
+    },
+    extend: {
+      colors: {
+        beige: "#F7F7F7",
+        orange: "#FFA500",
+        red: "#800000",
+        yellow: "#FFD700",
+        brown: "#A0522D",
+        green: "#008000",
+        black: "#333333"
+      }
+    },
   },
   plugins: [],
 }
