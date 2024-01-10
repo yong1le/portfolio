2ef90785455f50361fc440a9c71301be0258f458
yong1le <ygle.he@gmail.com>
2024-01-10 13:05:39 -0500

    disable animation on mobile

--- app/components/Utils/FadeIn.tsx
+++ app/components/Utils/FadeIn.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { useSpring, animated } from "@react-spring/web";
+import { isMobile } from "react-device-detect";
 import { InView } from "react-intersection-observer";
 
 const FadeIn = ({ children }: { children: React.ReactNode }) => {
   const [fade, api] = useSpring(() => ({
-    from: { opacity: 0, transform: `translate(0, 0%)` },
+    from: {
+      opacity: isMobile ? 1 : 0,
+      transform: isMobile ? "" : `translate(0, 0%)`,
+    },
   }));
 
   const handleViewChange = (
@@ -23,19 +27,27 @@ const FadeIn = ({ children }: { children: React.ReactNode }) => {
       api.start({
         from: { opacity: 0, transform: `translate(0,${fadeDir})` },
         to: { opacity: 1, transform: "translate(0, 0%)" },
-        config: { duration: 700 },
+        config: { duration: 600 },
       });
     } else {
       api.start({
         from: { opacity: 1, transform: "translate(0, 0%)" },
         to: { opacity: 0, transform: `translate(0, ${fadeDir})` },
-        config: { duration: 700 },
+        config: { duration: 300 },
       });
     }
   };
 
   return (
-    <InView as="div" onChange={handleViewChange} threshold={0.4}>
+    <InView
+      as="div"
+      onChange={(inView, entry) => {
+        if (!isMobile) {
+          handleViewChange(inView, entry);
+        }
+      }}
+      threshold={0.4}
+    >
       <animated.div style={fade}>{children}</animated.div>
     </InView>
   );
